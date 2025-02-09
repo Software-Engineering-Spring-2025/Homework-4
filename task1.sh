dd6c66f95477d39e866b6b9bbdc32ed7b9694abd
jillahmad17 <jillahmad17@gmail.com>
2025-02-09 10:38:44 -0500

    fixed task 1... I think

--- task1.sh
+++ task1.sh
@@ -1,4 +1,4 @@
 # edit task1.sh...
  sh infinite.sh
- your script  # run ehere to see what your script produces
- kill -9 $(ps | ...) # run the same script here to see what it produces
+ 
+ kill -9 $(ps aux | grep "infinite.sh" | grep -v grep | awk '{print $2}') # run the same script here to see what it producesps
