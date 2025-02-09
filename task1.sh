93b3955a497eda7cdb387e5ea89fea1b4769a62a
jillahmad17 <jillahmad17@gmail.com>
2025-02-08 20:06:31 -0500

    adding tasks 1 and 2

---
+++ task1.sh
@@ -0,0 +1,4 @@
+# edit task1.sh...
+ sh infinite.sh
+ your script  # run ehere to see what your script produces
+ kill -9 $(ps | ...) # run the same script here to see what it produces
---
+++ task2.sh
@@ -0,0 +1,21 @@
+#Find all lines containing "banana":
+grep banana sample.txt 
+printf "######################################\n"
+
+#Find lines that do not contain "banana":
+grep -v banana sample.txt
+printf "######################################\n"
+
+#Use uniq to find unique lines (output should be the above file, with only one line each for apple, banaona, cherry, date)
+sort sample.txt | uniq 
+printf "######################################\n"
+
+#run the command man uniq. Use what you found to extend the uniq comment to count the occurange of each work
+#man uniq sample.txt
+
+# Step 1. Use what you found to extend the uniq comment to count the occurange of each work
+sort sample.txt | uniq -c  
+
+#Combine grep and uniq to count occurrences of lines containing "banana":
+
+grep banana sample.txt | uniq -c
